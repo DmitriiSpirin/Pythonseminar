505fcbc71457165619958bf7cb650db4fcc72b7c
Dmitrii <666666667@mail.ru>
2023-02-06 19:16:59 +0300

    seminar2

---
+++ seminar2.py
@@ -0,0 +1,25 @@
+
+# n = int(input())
+# k = 0
+# for i in range(n):
+#     v = int(input())
+#     if v == 1:
+#         k += 1
+# print(k if k < n/2 else n-k)
+
+# print("Введите число S")
+# S = int(input())
+# print("Введите число P")
+# P = int(input())
+# for num1 in range(S-1):
+#     num2 = S - num1
+#     if num1*num2 == P:
+#         print(num1, num2)
+#         break
+
+
+# n=int(input())
+# p=1
+# while p<=n:
+#     print(p,end=' ')
+#     p=p*2

4b9adeb913dc95bf123c378a0caa4863f679aa7b
Dmitrii <666666667@mail.ru>
2023-03-09 17:45:18 +0300

    comleted

---
+++ seminar7.py
@@ -0,0 +1,24 @@
+# def poems(str):
+#     str = str.split()
+#     list_1 = []
+#     for word in str:
+#         sum = 0
+#         for i in word:
+#             if i in 'аеёиоуыэюя':
+#                 sum += 1
+#         list_1.append(sum)
+#     return len(list_1) == list_1.count(list_1[0])
+#
+# text = input("Введите фразу: ")
+# if poems(text):
+#     print('Парам пам-пам')
+# else:
+#     print('Пам парам')
+
+def print_operation_table(operation, num_rows, num_сolumns):
+    arr=[[operation(i,j) for i in range(1,num_rows+1)] for j in range(1, num_сolumns+1)]
+    for i in arr:
+        print(*[f"{x:>3}"for x in i])
+line = int(input("Введите количество строк: "))
+columns = int(input("Введите количество столбцов: "))
+print_operation_table(lambda x,y: x*y,line,columns)

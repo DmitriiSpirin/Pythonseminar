4cc4643ffa282fad1dad53cec0e7e0981521fc5e
Dmitrii <666666667@mail.ru>
2023-05-03 12:54:31 +0300

    added task

--- seminar5.py
+++ seminar5.py
@@ -1,8 +1,21 @@
-def degreeAB(a, b):
-    if b == 0:
-        return 1
-    return a * degreeAB(a, b - 1)
+# def degreeAB(a, b):
+#     if b == 0:
+#         return 1
+#     return a * degreeAB(a, b - 1)
+#
+# a = int(input('Введите число: '))
+# b = int(input('Введите степень: '))
+# print(degreeAB(a, b))
 
-a = int(input('Введите число: '))
-b = int(input('Введите степень: '))
-print(degreeAB(a, b))
+a = int(input("Введите первое неотрицительное число "))
+b = int(input("Введите второе неотрицательно число "))
+
+
+def recursive_sum(a, b):
+    if a == 0:
+        return b
+    else:
+        return recursive_sum(a - 1, b + 1)
+
+
+print(recursive_sum(a, b))
